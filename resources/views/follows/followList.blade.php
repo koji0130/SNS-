296b165723725ac0d62b3ce5ca18517f509aeb3b
山崎浩司 <xzsggq789p@privaterelay.appleid.com>
2022-04-23 12:25:03 +0900

    no message

--- resources/views/follows/followList.blade.php
+++ resources/views/follows/followList.blade.php
@@ -3,15 +3,23 @@
 
 <h2 class="list-title">Follow List</h2>
 <div class="icons">
-  <div class="icon-list">@foreach ($user as $list) @if ($list->id != Auth::user()->id) @if (auth()->user()->isFollowing($list->id))
+  <div class="icon-list">
+    @foreach ($user as $list) @if ($list->id != Auth::user()->id)
+    @if (auth()->user()->isFollowing($list->id))
     <div class="profileImage">
       <a href="otherUser/{{$list->id}}">
         <img src="{{ asset('storage/images/' . $list->images) }}">
       </a>
-    </div>@endif @endif @endforeach
+    </div>
+    @endif
+    @endif
+    @endforeach
   </div>
 </div>
-<div class="posts-index">@foreach ($post as $list) @if (Auth::user()->isFollowing($list->user->id) ) @if ($list->user->id != Auth::user()->id)
+<div class="posts-index">
+  @foreach ($post as $list)
+  @if (Auth::user()->isFollowing($list->user->id) )
+  @if ($list->user->id != Auth::user()->id)
   <div class="posts">
     <div class="image-posts">
       <div class="icon-group">
@@ -23,11 +31,15 @@
         <div class="username">
           <p>{{ $list->user->username }}</p>
         </div>
-        <div class="post">{{ $list->post }}</div>
+        <div class="post">
+          {{ $list->post }}
+        </div>
       </div>
     </div>
     <div class="button-group">
-      <div class="timestamp">{{ $list->created_at }}</div>
+      <div class="timestamp">
+        {{ $list->created_at }}
+      </div>
     </div>
   </div>
   @endif
